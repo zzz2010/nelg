66ce4223a5fdface2f53e5bf510dce28f9818e59
zzz2010@gmail.com <zzz2010@gmail.com@baefe69e-d4af-2cd5-41e0-625deadff523>
2012-10-03 03:14:54 +0000

    zzz

---
+++ NELG/src/NELGMain.java
@@ -0,0 +1,22 @@
+import org.apache.commons.cli.Options;
+
+
+public class NELGMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Options options = new Options();
+		options.addOption("threadnum", true, "min count for extention");
+		
+		//Library Loading
+		//load cell line
+		
+		//load general track
+		
+		//phase1 
+	}
+
+}
