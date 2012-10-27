d44b1cf11579ffdc679e4400a5f641342c84ef16
zzz2010@gmail.com <zzz2010@gmail.com@baefe69e-d4af-2cd5-41e0-625deadff523>
2012-10-27 07:51:59 +0000

    enable debug mode

--- trunk/NELG/src/common.java
+++ trunk/NELG/src/common.java
@@ -5,4 +5,6 @@ public class common {
 	static String tempDir="./cache/";
 	static int SignalRange=10000;
 	static int MinimumPeakNum=50;
+	static String predictTarget_debug="";
+	static String selectFeature_debug="";
 }
