5c7308a7c20fc2c036192b3e45aadf1ffed6fe5d
zzz2010@gmail.com <zzz2010@gmail.com@baefe69e-d4af-2cd5-41e0-625deadff523>
2012-10-29 05:35:25 +0000

    back to threadpool from jppf, and improve Synonym check

--- NELG/src/SynonymCheck.java
+++ NELG/src/SynonymCheck.java
@@ -3,10 +3,27 @@ public class SynonymCheck {
 
 	static boolean isSynonym(TrackRecord t1, TrackRecord t2)
 	{
-		if(t1.ExperimentId==t2.ExperimentId)
-			return true;
+
+		
 		if(t1.ExperimentType.contains("Cage")&&t2.ExperimentType.contains("Cage"))
 			return true;
+		
+		if(firstCapital(t1.ExperimentId).equalsIgnoreCase(firstCapital(t2.ExperimentId)))
+			return true;
+		
+		
 		return false;
+	}
+	
+	static String firstCapital(String input)
+	{
+		String out=input.substring(0,1);
+		int i=1;
+		for ( i = 1; i < input.length(); i++) {
+			if(Character.isUpperCase(input.charAt(i)))
+				break;
+		}
+		out+=input.substring(1,i);
+		return out;
 	}
 }
