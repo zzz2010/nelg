df49f334ddc54b976da401fad5449b5ea00682a4
zzz2010@gmail.com <zzz2010@gmail.com@baefe69e-d4af-2cd5-41e0-625deadff523>
2012-11-03 13:44:44 +0000

    add score support for intervaltree

--- trunk/NELG/src/BEDIntervalIndex.java
+++ trunk/NELG/src/BEDIntervalIndex.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.TreeMap;
 
 import org.broad.tribble.bed.BEDCodec;
 
@@ -11,30 +12,40 @@ import org.broad.tribble.index.interval.IntervalTree;
 public class BEDIntervalIndex {
 
 	LinkedHashMap<String, IntervalTree> indexMap;
+	LinkedHashMap<String, TreeMap<Integer, Float>> valMap;
+	
 
 	public BEDIntervalIndex(String bedfile) {
 		List<SimpleBEDFeature> rawbed=FileStorageAdapter.getBEDData(bedfile);
 		indexMap=new LinkedHashMap<String, IntervalTree>();
+		valMap=new LinkedHashMap<String, TreeMap<Integer,Float>>();
 		for (int i = 0; i < rawbed.size(); i++) {
 			SimpleBEDFeature temp=rawbed.get(i);
 			if(!indexMap.containsKey(temp.getChr()))
 			{
 				indexMap.put(temp.getChr(), new IntervalTree());
+				valMap.put(temp.getChr(), new TreeMap<Integer, Float>());
 			}
 			indexMap.get(temp.getChr()).insert(new Interval(temp.getStart(), temp.getEnd(),null));
+			int midpos=(temp.getStart()+temp.getEnd())/2;
+			valMap.get(temp.getChr()).put(midpos, temp.getScore());
 		}
 		
 	}
 	
 	public BEDIntervalIndex(List<SimpleBEDFeature> rawbed) {
 		indexMap=new LinkedHashMap<String, IntervalTree>();
+		valMap=new LinkedHashMap<String, TreeMap<Integer,Float>>();
 		for (int i = 0; i < rawbed.size(); i++) {
 			SimpleBEDFeature temp=rawbed.get(i);
 			if(!indexMap.containsKey(temp.getChr()))
 			{
 				indexMap.put(temp.getChr(), new IntervalTree());
+				valMap.put(temp.getChr(), new TreeMap<Integer, Float>());
 			}
 			indexMap.get(temp.getChr()).insert(new Interval(temp.getStart(), temp.getEnd(),null));
+			int midpos=(temp.getStart()+temp.getEnd())/2;
+			valMap.get(temp.getChr()).put(midpos, temp.getScore());
 		}
 		
 	}
@@ -45,7 +56,9 @@ public class BEDIntervalIndex {
 		List<Interval> iv = indexMap.get(chr).findOverlapping(new Interval(start, end));
 		List<SimpleBEDFeature> ret=new ArrayList<SimpleBEDFeature>(iv.size());
 		for (int i = 0; i < iv.size(); i++) {			
-			ret.add(interval2BEDFeature(iv.get(i), chr));
+			SimpleBEDFeature temp = interval2BEDFeature(iv.get(i), chr);
+			temp.setScore(valMap.get(chr).get((temp.getStart()+temp.getEnd())/2));
+			ret.add(temp);
 		}
 		
 		return ret;
@@ -57,6 +70,18 @@ public class BEDIntervalIndex {
 		BEDCodec coder=new BEDCodec();
 		List<Interval> iv = indexMap.get(chr).findOverlapping(new Interval(start, end));
 		return iv.size();
+		
+	}
+	
+	public float overlapSumValue(String chr, int start, int end)
+	{
+		float sum=0;
+		List<SimpleBEDFeature> list = getBlocks(chr,start,end);
+		for (SimpleBEDFeature simpleBEDFeature : list) {
+			sum+=simpleBEDFeature.getScore();
+		}
+		
+		return sum;
 		
 	}
 	
