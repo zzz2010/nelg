398812fc96d616a40c1bec1b44286130ec3e23a9
zzz2010@gmail.com <zzz2010@gmail.com@baefe69e-d4af-2cd5-41e0-625deadff523>
2013-05-30 07:27:26 +0000

    fix bug

--- NELG/src/ChildModeler.java
+++ NELG/src/ChildModeler.java
@@ -143,16 +143,16 @@ public class ChildModeler {
 			
 		
 		int crossValNum=5;
-
+		if(common.printMode)
+			bestModeler=modelerSet.get(0);
+		else
 		for (int i = 0; i < modelerSet.size(); i++) {
 			
 		
 		Classifier modeler = modelerSet.get(i);
 		try {
 			Evaluation eval1 = new Evaluation(data2);
-			if(common.printMode)
-				eval1.evaluateModel(modeler, data2);
-			else
+
 				eval1.crossValidateModel(modeler, data2, crossValNum, new Random(1));
 			auc=eval1.areaUnderROC(1);
 			logger.debug(modeler.getClass().getName()+" auc:"+auc);
@@ -285,16 +285,15 @@ public class ChildModeler {
 			
 		
 		int crossValNum=5;
-
+		if(common.printMode)
+			bestModeler=modelerSet.get(0);
+		else
 		for (int i = 0; i < modelerSet.size(); i++) {
 			
 
 		Classifier modeler = modelerSet.get(i);
 		try {
 			Evaluation eval = new Evaluation(data2);
-			if(common.printMode)
-				eval.evaluateModel(modeler, data2);
-			else
 				eval.crossValidateModel(modeler, data2, crossValNum, new Random(1));
 			corr=eval.correlationCoefficient();
 			logger.debug(modeler.getClass().getName()+" corr:"+corr);
