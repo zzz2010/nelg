0262daed950ee8a6251027fac23f1b589c1b10bb
zzz2010@gmail.com <zzz2010@gmail.com@baefe69e-d4af-2cd5-41e0-625deadff523>
2013-07-03 07:50:49 +0000

    debug

--- NELG/src/PeakClassifier.java
+++ NELG/src/PeakClassifier.java
@@ -131,7 +131,7 @@ public class PeakClassifier {
 
 			       for(int i=1; i<dataset.getSeries().size();i+=2)
 			       {
-			    	   		renderer.setSeriesFillPaint(i, renderer.getSeriesPaint(i-1));
+			    	   		renderer.setSeriesFillPaint(i, renderer.lookupSeriesPaint(i-1));
 			    	   		renderer.setSeriesStroke(i, 
 			    	   	            new BasicStroke(
 			    	   	                2.0f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 
