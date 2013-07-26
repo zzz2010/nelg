c0a81786bbcfaba895c52a5c7136544b763548fe
zzz2010@gmail.com <zzz2010@gmail.com@baefe69e-d4af-2cd5-41e0-625deadff523>
2013-07-26 03:19:06 +0000

    test

--- trunk/NELG/src/NELGViewResult.java
+++ trunk/NELG/src/NELGViewResult.java
@@ -592,7 +592,7 @@ public class NELGViewResult {
 				double max=sortedVec.getQuick(stridesize-3);
 			
 				double pesudoCnt=Math.max(0.1, Math.min(max/2, 1));
-
+				median=1;
 				if(median==max)
 					median=0.11;  //no need to normalize
 				else if(median<1)
