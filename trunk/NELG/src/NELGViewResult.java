9e15e29550d4ed9b7af61f959b35036dcd2c218c
zzz2010@gmail.com <zzz2010@gmail.com@baefe69e-d4af-2cd5-41e0-625deadff523>
2013-07-26 04:48:39 +0000

    set median threshold to 50

--- trunk/NELG/src/NELGViewResult.java
+++ trunk/NELG/src/NELGViewResult.java
@@ -604,8 +604,8 @@ public class NELGViewResult {
 				median=1;
 				if(median==max)
 					median=0.11;  //no need to normalize
-				else if(median<5)
-					median=5;
+				else if(median<50)
+					median=50;
 				if(Double.isNaN(median))
 					median=1;
 				if(median>-2)
